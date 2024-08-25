12acbca12d3e03718df07a9b3ac7832733083a12
Matheus-Ryan <130509088+Matheus-Ryan@users.noreply.github.com>
2024-08-24 23:38:51 -0300

    Update viewsets.py

---
+++ biblioteca/autores/viewsets.py
@@ -0,0 +1,7 @@
+from rest_framework import viewsets
+from .models import Autor
+from .serializers import AutorSerializer
+
+class AutorViewSet(viewsets.ModelViewSet):
+    queryset = Autor.objects.all()
+    serializer_class = AutorSerializer
