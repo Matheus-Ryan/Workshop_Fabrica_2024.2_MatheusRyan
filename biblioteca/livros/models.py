5c99b4e0a85074067b348b395ced7576981523c1
Matheus-Ryan <130509088+Matheus-Ryan@users.noreply.github.com>
2024-08-24 23:41:10 -0300

    Update models.py

--- biblioteca/livros/models.py
+++ biblioteca/livros/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from autores.models import Autor
 
 # Create your models here.
+
+class Livro(models.Model):
+    titulo = models.CharField(max_length=200)
+    autor = models.ForeignKey(Autor, on_delete=models.CASCADE)
+    isbn = models.CharField(max_length=13, unique=True)
+    
+    def __str__(self):
+        return self.titulo
